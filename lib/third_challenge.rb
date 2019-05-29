9381d2ba4204d27b0b109740a32b23e3e423d320
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 21:04:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         :name => "Romeo",
         :age => "15",
-        :status: "alive"
+        :status => "alive"
         },
       :hero_friends => []
    },
@@ -32,7 +32,7 @@ def third_challenge
         #your key/value pairs here
         :name => "Juliet",
         :age => "15",
-        :status: "alive"
+        :status => "alive"
         },
       :heroine_friends => []
    }
