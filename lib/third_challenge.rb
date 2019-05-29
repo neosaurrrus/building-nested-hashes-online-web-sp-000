696c541744be5685e90235b3395e42b738acb0dc
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 21:21:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,25 +1,13 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        :name => "Lady Montague",
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
-        :hero_friends => [
-            {name: "Benvolio", age: "17", attitude: "worried"},
-            {name: "Mercutio", age: "18", attitude: "hot-headed"}
-          ]
+    :montague => {
+      :patriarch => {name: "Michael Jordan", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    },
    :capulet => {
       :patriarch => {
