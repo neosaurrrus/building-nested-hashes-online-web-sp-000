e0f7a4882188df1c8ef3968c1614d461fc1ba2f3
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 21:07:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -25,7 +25,7 @@ def third_challenge
           :name => "Mercutio"
           :age => "18"
           :attitude: "hot-headed"
-          }]
+        }]
    },
    :capulet => {
       :patriarch => {
