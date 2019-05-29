5dde53ef415fbf3ed96f24cff89714d0296772ca
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 21:33:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,7 +20,7 @@ def fourth_challenge
    }
 }
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+# epic_tragedy[:montague][:hero][:status] = "dead"
+# epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 end
